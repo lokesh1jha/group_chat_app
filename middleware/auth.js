92f157c26204de2e3ce77e7e7d95d243ec393ca1
lokesh1jha <lokesh1jha@gmail.com>
2022-09-25 18:43:37 +0530

    middleware

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+    try {
+        const tocken = req.header('Authorization');
+        console.log('tocken : ' + tocken);
+        const userid = Number(jwt.verify(tocken, process.env.TOCKEN_SECRET));
+        User.findByPk(userid).then(user => {
+            console.log(JSON.stringify(user));
+            req.user = user;
+            next();
+        })
+        .catch(err => { throw new Error(err) })
+    } catch (err) {
+        console.log(err);
+        return res.status(401).json({ success: false })
+    };
+}
+
+module.exports = {
+    authenticate
+}
